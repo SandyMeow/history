75324232df2aeb5378191620cbc77e1671abdfe7
SandyMeow <740440626@qq.com>
2017-11-01 14:29:45 +0800

    categorychange

---
+++ category.php
@@ -0,0 +1,160 @@
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>课程分类</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<meta name="renderer" content="webkit">
+<meta http-equiv="Cache-Control" content="no-siteapp" />
+<link rel="stylesheet" href="amazeui/css/amazeui.min.css"/>
+<link rel="stylesheet" href="default/style.css"/>
+<script src="amazeui/js/jquery.min.js"></script>
+<script src="amazeui/js/amazeui.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+	<header data-am-widget="header" class="am-header am-header-default my-header">
+      <div class="am-header-left am-header-nav">
+        <a href="index.html" class="">
+          <i class="am-header-icon am-icon-chevron-left"></i>
+        </a>
+      </div>
+      <h1 class="am-header-title">
+        <a href="#title-link" class="">课程分类</a>
+      </h1>
+<!--
+      <div class="am-header-right am-header-nav">
+        <a href="#right-link" class="">
+          <i class="am-header-icon  am-icon-home"></i>
+        </a>
+      </div>
+-->
+    </header>
+    
+    <div class="gray-panel">
+    	<div class="my-search-title-panel">
+        	<div class="am-input-group">
+              <input type="text" class="am-form-field am-radius">
+              <span class="am-input-group-btn">
+                <button class="am-btn am-radius" type="button"><span class="am-icon-search"></span></button>
+              </span>
+            </div>
+        </div>
+    </div>
+    	<section data-am-widget="accordion" class="am-accordion am-accordion-gapped"
+            data-am-accordion='{"multiple": true}'>
+              <dl class="am-accordion-item am-active">
+                <dt class="am-accordion-title">学习交流</dt>
+                <dd class="am-accordion-bd am-collapse am-in">
+                  <!-- 规避 Collapase 处理有 padding 的折叠内容计算计算有误问题， 加一个容器 -->
+                  <div class="am-accordion-content">
+                  	<dl class="my-category-dl">
+                    	<dt><a href="#" class="red2 am-cf">必修课</a></dt>
+                        <dd><a href="#">微积分</a></dd>
+                        <dd><a href="#">概率统计</a></dd>
+                        <dd><a href="#">线性代数</a></dd><dd><a href="#">1</a></dd><dd><a href="#">2</a></dd><dd><a href="#">3</a></dd><dd><a href="#">4</a></dd>
+                    </dl>
+                    <dl class="my-category-dl">
+                    	<dt><a href="#" class="red2 am-cf">选修课</a></dt>
+                        <dd><a href="#">ps</a></dd>
+                        <dd><a href="#">视频剪辑</a></dd>
+                        <dd><a href="#">微信推送</a></dd><dd><a href="#">1</a></dd><dd><a href="#">2</a></dd><dd><a href="#">3</a></dd><dd><a href="#">4</a></dd>
+                    </dl>
+                  </div>
+                </dd>
+              </dl>
+              <dl class="am-accordion-item">
+                <dt class="am-accordion-title">文体技能交流</dt>
+                <dd class="am-accordion-bd am-collapse ">
+                  <!-- 规避 Collapase 处理有 padding 的折叠内容计算计算有误问题， 加一个容器 -->
+                  <div class="am-accordion-content">
+                  	 <dl class="my-category-dl">
+                    	<dt><a href="#" class="red2 am-cf">体育</a></dt>
+                        <dd><a href="#">排球</a></dd>
+                        <dd><a href="classincategory.php?class_category=篮球">篮球</a></dd>
+                        <dd><a href="#">跑步</a></dd><dd><a href="#">引体向上</a></dd><dd><a href="#">立定跳远</a></dd><dd><a href="#">仰卧起坐</a></dd><dd><a href="#">减肥</a></dd>
+                    </dl>
+                    <dl class="my-category-dl">
+                    	<dt><a href="#" class="red2 am-cf">文艺</a></dt>
+                        <dd><a href="#">唱歌</a></dd>
+                        <dd><a href="#">跳舞</a></dd>
+                        <dd><a href="#">吉他</a></dd><dd><a href="#">长笛</a></dd><dd><a href="#">尤克里里</a></dd><dd><a href="#">表演</a></dd><dd><a href="#">形体</a></dd>
+                    </dl>
+                  </div>
+                </dd>
+              </dl>
+              <dl class="am-accordion-item">
+                <dt class="am-accordion-title">生活技能交流</dt>
+                <dd class="am-accordion-bd am-collapse ">
+                  <!-- 规避 Collapase 处理有 padding 的折叠内容计算计算有误问题， 加一个容器 -->
+                  <div class="am-accordion-content">
+                  	<dl class="my-category-dl">
+                    	<dt><a href="#" class="red2 am-cf">美妆</a></dt>
+                        <dd><a href="#">裸妆</a></dd>
+                        <dd><a href="#">韩妆</a></dd>
+                        <dd><a href="#">欧美妆</a></dd><dd><a href="#">舞台妆</a></dd>
+                    </dl>
+                    <dl class="my-category-dl">
+                    	<dt><a href="#" class="red2 am-cf">家政</a></dt>
+                        <dd><a href="#">做饭</a></dd>
+                        <dd><a href="#">打扫</a></dd>
+                        <dd><a href="#">收纳</a></dd>
+                    </dl>
+                  </div>
+                </dd>
+              </dl>
+            </section>
+    <div data-am-widget="gotop" class="am-gotop am-gotop-fixed">
+      <a href="#top" title="">
+        <img class="am-gotop-icon-custom" src="default/goTop.gif" />
+      </a>
+    </div>
+    <footer data-am-widget="footer" class="am-footer am-footer-default" data-am-footer="{  }">
+        <hr data-am-widget="divider" style="" class="am-divider am-divider-default"/>
+      <div class="am-footer-miscs ">
+        <p></p>
+        <p></p>
+      </div>
+    </footer>
+    <!--底部-->
+    <div data-am-widget="navbar" class="am-navbar am-cf my-nav-footer " id="">
+      <ul class="am-navbar-nav am-cf am-avg-sm-4 my-footer-ul">
+        <li>
+          <a href="index.html" class="">
+            <span class="am-icon-home"></span>
+            <span class="am-navbar-label">首页</span>
+          </a>
+        </li>
+        <li>
+          <a href="publish.html" class="">
+            <span class=" am-icon-comments"></span>
+            <span class="am-navbar-label">信息发布</span>
+          </a>
+        </li>
+        <li>
+          <a href="favorite.html" class="">
+            <span class="am-icon-user"></span>
+            <span class="am-navbar-label">收藏夹</span>
+          </a>
+        </li>
+        <li>
+          <a href="resume.html" class="">
+            <span class="am-icon-user"></span>
+            <span class="am-navbar-label">我的</span>
+          </a>
+        </li>
+      </ul>
+      <script>
+	  	function showFooterNav(){
+			$("#footNav").toggle();
+		}
+	  </script>
+    </div>
+</div>
+</body>
+</html>
+
---
+++ classincategory.php
@@ -0,0 +1,250 @@
+<?php
+
+include("connect3.php");
+//接收数据
+    class input{
+		function post($key){
+
+        if(isset($_POST[$key]) === FALSE){
+
+            return FALSE;
+		}
+			$val = $_POST[ $key ];
+			return $val;
+		}
+        function get($key){
+            if(isset($_GET[$key]) === FALSE){
+				return FALSE;
+			}
+			$val = $_GET[$key];
+			return $val;
+		}
+	}
+$input=new input();
+$class_category=$input->get('class_category');
+$table="class_category";
+echo($class_category);
+
+//设定查询语句
+$myquery1="SELECT * FROM `class_category` WHERE `class_category` LIKE '$class_category'";
+$result1=$link->query($myquery1);
+
+if($result1){
+	echo"成功";
+}
+else echo"失败";
+
+$row1=$result1->fetch_array();
+echo $row1["class_name"];
+
+
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>首页</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<meta name="renderer" content="webkit">
+<meta http-equiv="Cache-Control" content="no-siteapp" />
+<link rel="stylesheet" href="amazeui/css/amazeui.min.css"/>
+<link rel="stylesheet" href="default/style.css"/>
+<script src="amazeui/js/jquery.min.js"></script>
+<script src="amazeui/js/amazeui.min.js"></script>
+</head>
+
+<body>
+<div class="container">
+	<header data-am-widget="header" class="am-header am-header-default my-header">
+      <div class="am-header-left am-header-nav am-dropdown"  data-am-dropdown>
+        <a href="javascript:;" class="am-dropdown-toggle"  data-am-dropdown-toggle>
+          <i class="am-header-icon am-icon-th-large"></i>
+        </a>
+        <div style="display:none">
+
+        </div>
+        <ul class="am-dropdown-content">
+           <li><a href="#">我的历史</a></li>
+            <li><a href="#">个人信息</a></li>
+            <li><a href="#">我的简历</a></li>
+            <li><a href="#">商城</a></li>
+            <li><a href="#">订单成交</a></li>
+            <li><a href="#">相册</a></li>
+            <li><a href="#">视频</a></li>
+          </ul>
+      </div>
+      <h1 class="am-header-title">
+        <a href="#title-link" class="">教你呀</a>
+      </h1>
+      <div class="am-header-right am-header-nav">
+        <a href="#right-link" class="">
+          <i class="am-header-icon  am-icon-share-alt"></i>
+        </a>
+      </div>
+    </header>
+    <!-- banner -->
+    <div data-am-widget="slider" class="am-slider am-slider-a1" data-am-slider='{&quot;directionNav&quot;:false}'>
+      <ul class="am-slides">
+        <li>
+          <img src="default/ggb.jpg">
+        </li>
+        <li>
+          <img src="default/ggb.jpg">
+        </li>
+        <li>
+          <img src="default/ggb.jpg">
+        </li>
+        <li>
+          <img src="default/ggb.jpg">
+        </li>
+      </ul>
+    </div>
+    <div class="am-cf am-g my-shop-nav">
+    	<div class="my-shop-nav-panellf">
+        	<div class=" my-shop-nav-img am-circle ">
+        	<img src="default/img1.jpg" class="am-circle m-img-thumbnail" />
+            </div>
+        </div>
+        <div class="my-shop-nav-panelrt am-fr">
+        	<ul class="am-nav am-nav-pills my-shop-nav-ul">
+            	<li><a href="#">信息平台</a></li>
+                <li><a href="category.html">课程分类</a></li>
+                <li><a href="#">我的收藏</a></li>
+
+            </ul>
+        </div>
+    </div>
+    <div class="am-cf am-g">
+    	<ul class="am-avg-sm-2 my-shop-product-list">
+        	<li>
+            	<div class="am-panel am-panel-default">
+                    <div class="am-panel-bd">
+                    	<img class="am-img-responsive" src="default/timg.jpg" />
+                                        <h3><a href="#">篮球期末考试辅导</a></h3>
+        <div>
+                            <span class="list-product-price-span">￥30.00</span>
+                            <span class="list-product-commission-span">佣金<br/>2.84</span>
+                            <span class="list-product-sorce-span">积分<br/>4</span>
+                        </div>
+                        <hr data-am-widget="divider" style="" class="am-divider am-divider-default am-cf"/>
+                        <ol class="am-avg-sm-3 product-list-share">
+                        	<li><a href="#"><img src="default/icon1.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon2.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon3.png" class="am-img-responsive" /></a></li>
+                        </ol>
+                    </div>
+                </div>
+            </li>
+            <li>
+            	<div class="am-panel am-panel-default">
+                    <div class="am-panel-bd">
+                    	<img class="am-img-responsive" src="default/pq.jpg" />
+                        <h3><a href="#">排球期末考试辅导</a></h3>
+                        <div>
+                            <span class="list-product-price-span">￥30.00</span>
+                            <span class="list-product-commission-span">佣金<br/>2.84</span>
+                            <span class="list-product-sorce-span">积分<br/>4</span>
+                        </div>
+                        <hr data-am-widget="divider" style="" class="am-divider am-divider-default am-cf"/>
+                        <ol class="am-avg-sm-3 product-list-share">
+                        	<li><a href="#"><img src="default/icon1.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon2.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon3.png" class="am-img-responsive" /></a></li>
+                        </ol>
+                    </div>
+                </div>
+            </li>
+            <li>
+            	<div class="am-panel am-panel-default">
+                    <div class="am-panel-bd">
+                    	<img class="am-img-responsive" src="default/hz.jpg" />
+                        <h3><a href="#">学生裸妆教学</a></h3>
+                        <div>
+                            <span class="list-product-price-span">￥40.00</span>
+                            <span class="list-product-commission-span">佣金<br/>2.84</span>
+                            <span class="list-product-sorce-span">积分<br/>4</span>
+                        </div>
+                        <hr data-am-widget="divider" style="" class="am-divider am-divider-default am-cf"/>
+                        <ol class="am-avg-sm-3 product-list-share">
+                        	<li><a href="#"><img src="default/icon1.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon2.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon3.png" class="am-img-responsive" /></a></li>
+                        </ol>
+                    </div>
+                </div>
+            </li>
+            <li>
+            	<div class="am-panel am-panel-default">
+                    <div class="am-panel-bd">
+                    	<img class="am-img-responsive" src="default/cyy.jpg" />
+                        <h3><a href="#">C语言辅导</a></h3>
+                        <div>
+                            <span class="list-product-price-span">￥89.00</span>
+                            <span class="list-product-commission-span">佣金<br/>2.84</span>
+                            <span class="list-product-sorce-span">积分<br/>4</span>
+                        </div>
+                        <hr data-am-widget="divider" style="" class="am-divider am-divider-default am-cf"/>
+                        <ol class="am-avg-sm-3 product-list-share">
+                        	<li><a href="#"><img src="default/icon1.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon2.png" class="am-img-responsive" /></a></li>
+                            <li><a href="#"><img src="default/icon3.png" class="am-img-responsive" /></a></li>
+                        </ol>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    
+    
+    <footer data-am-widget="footer" class="am-footer am-footer-default" data-am-footer="{  }">
+        <hr data-am-widget="divider" style="" class="am-divider am-divider-default"/>
+    </footer>
+    <!--底部-->
+    <div data-am-widget="navbar" class="am-navbar am-cf my-nav-footer " id="">
+      <ul class="am-navbar-nav am-cf am-avg-sm-4 my-footer-ul">
+        <li>
+          <a href="/wap/" class="">
+            <span class="am-icon-home"></span>
+            <span class="am-navbar-label">首页</span>
+          </a>
+        </li>
+        <li>
+          <a href="###" class="">
+            <span class=" am-icon-comments"></span>
+            <span class="am-navbar-label">信息发布</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="">
+            <span class=" am-icon-map-marker"></span>
+            <span class="am-navbar-label">地图</span>
+          </a>
+        </li>
+        <li style="position:relative">
+          <a href="javascript:;" onClick="showFooterNav();" class="">
+            <span class="am-icon-user"></span>
+            <span class="am-navbar-label">个人</span>
+          </a>
+          <div class="footer-nav" id="footNav">
+          	<span class=" am-icon-bank"><a href="#">教授中心</a></span>
+            <span class="am-icon-suitcase"><a href="#">学习中心</a></span>
+            <span class="am-icon-usd"><a href="#">我的钱包</a></span>
+            <span class="am-icon-user"><a href="#">个人资料</a></span>
+            <span class="am-icon-th-list"><a href="#">帮助中心</a></span>
+            <span class="am-icon-comments"><a href="#">消息中心</a></span>
+            <span class="am-icon-power-off"><a href="#">安全退出</a></span>
+          </div>
+        </li>
+      </ul>
+      <script>
+	  	function showFooterNav(){
+			$("#footNav").toggle();
+		}
+	  </script>
+    </div>
+</div>
+</body>
+</html>
